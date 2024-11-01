ff4b98816f2aecc4a837cf4119f7b0a915e6ceaa
oscar-tyndale-dev <oscar.dykeman@tyndaleadvisors.com>
2024-11-01 14:11:21 -0700

    global.css method

--- gulpfile.js
+++ gulpfile.js
@@ -3,27 +3,27 @@ const sass = require('gulp-sass')(require('sass'));
 
 // Paths
 const paths = {
-   general: 'general-styles/**/*.scss',
-   siteSpecific: 'sites/**/*.scss',
+   global: 'scss/global.scss', // Entry point for your SCSS
+   allScss: 'scss/**/*.scss',  // Pattern to watch all SCSS files
    dist: 'dist/'
 };
 
 // Compile function
-function compileSiteStyles() {
-   return gulp.src(paths.siteSpecific)
+function compileGlobalStyles() {
+   return gulp.src(paths.global) // Compile only the global.scss file
       .pipe(sass({
-         includePaths: ['general-styles'],
+         includePaths: ['scss'],
          sourceMap: true
       }).on('error', function(err) {
          console.log(err.message);
          this.emit('end');
       }))
-      .pipe(gulp.dest(paths.dist));
+      .pipe(gulp.dest(paths.dist)) // Output directly to dist as global.css
 }
 
 // Watch function
 function watch() {
-   gulp.watch([paths.general, paths.siteSpecific], compileSiteStyles);
+   gulp.watch(paths.allScss, compileGlobalStyles); // Watch all SCSS files
 }
 
 // Clean task (optional but recommended)
@@ -33,4 +33,4 @@ function clean(cb) {
 }
 
 // Gulp default task
-exports.default = gulp.series(clean, compileSiteStyles, watch);
+exports.default = gulp.series(clean, compileGlobalStyles, watch);
